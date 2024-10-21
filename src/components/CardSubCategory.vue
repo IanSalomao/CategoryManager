cb922b20c8dd90f3e5881aa85167a95f3355d2c6
Ian_S <85195921+IanSalomao@users.noreply.github.com>
2024-10-20 23:20:41 -0300

    feat: componente CardSubCategory.vue

---
+++ src/components/CardSubCategory.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ subCategory.name }}</h5>
+      <p class="card-text">{{ subCategory.description }}</p>
+      <button class="btn btn-primary" @click="$emit('edit', subCategory.id)">Editar</button>
+      <button class="btn btn-danger" @click="$emit('delete', subCategory.id)">Deletar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardSubCategory",
+  props: {
+    subCategory: Object,
+  },
+};
+</script>
